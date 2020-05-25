efaeb0a94f327bb85d6326008ab0fe2e3177fb79
chisoftltd <chisoftltd@gmail.com>
2020-05-25 21:47:18 +0200

    Mathematical Functions Random Number Function Trig Functions

--- MathandNumber.py
+++ MathandNumber.py
@@ -1,8 +1,16 @@
 # Program by ChisoftMedia
+import math
+import random as rd
+import numpy as np
+
+
+from filecmp import cmp
 
 a = 456
 b = 100
 c = 0b100
+
+d = a/b
 
 print(c)
 
@@ -20,8 +28,64 @@ print(bin(y))
 
 # Convertion from binary to decimal
 i = 0b01010
-k = 0b0101110
+j = 0b0101110
+k = i/j
+
 
 print(i)
 
 print(k)
+
+print(float(i))
+
+print(complex(k))
+
+print(complex(i, k))
+
+
+# Mathematical Functions
+
+print(abs(complex(i, k)))
+
+print(math.ceil(k))
+
+print(math.exp(k))
+
+
+del k
+
+
+k = i >> 3
+
+
+print(k)
+
+print(i >> 2)
+
+print((math.floor(d)))
+
+print(math.pow(a, d))
+
+del a, b, c
+
+# Random Number Function
+
+
+a = rd.randrange(0, 11, 1)
+print(a)
+
+
+a = np.arange(0, 110, 1)
+print(a)
+
+# Trig Functions
+
+x = 0.64
+
+print(math.acos(x))
+
+print(math.atan(x))
+
+print(math.cos(x))
+
+print(math.radians(x))
