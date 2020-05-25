5ad93868e4f6107ccfcc025d72436ecb6dab9477
chisoftltd <chisoftltd@gmail.com>
2020-05-25 20:46:36 +0200

    Convertion from decimal to binary Convertion from binary to decimal

---
+++ MathandNumber.py
@@ -0,0 +1,27 @@
+# Program by ChisoftMedia
+
+a = 456
+b = 100
+c = 0b100
+
+print(c)
+
+
+# Convertion from decimal to binary
+x = 200
+y = 47
+
+z_bin = bin(x)
+
+print(z_bin)
+
+print(bin(y))
+
+
+# Convertion from binary to decimal
+i = 0b01010
+k = 0b0101110
+
+print(i)
+
+print(k)
